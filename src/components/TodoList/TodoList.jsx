ec4805a7a887e552df78ba4a4dfe912f1cd272a7
Lucy <chusu91@gmail.com>
2022-12-15 21:06:40 +0200

    first commit

---
+++ src/components/TodoList/TodoList.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import AddTodo from "../AddTodo/AddTodo";
+import Todo from "../Todo/Todo";
+
+export default function TodoList() {
+  const [todos, setTodos] = useState([
+    { id: "123", text: "shopping", status: "active" },
+    { id: "124", text: "studying", status: "active" },
+  ]);
+  const handleAdd = (todo) => {
+    setTodos([...todos, todo]);
+  };
+  const handleUpdate = (updated) =>
+    setTodos(todos.map((todo) => (todo.id === updated.id ? updated : todo)));
+  const handleDelete = (deleted) =>
+    setTodos(todos.filter((todo) => todo.id !== deleted.id));
+
+  return (
+    <section>
+      <ul>
+        {todos.map((item) => (
+          <Todo
+            key={item.id}
+            todo={item}
+            onUpdate={handleUpdate}
+            onDelete={handleDelete}
+          />
+        ))}
+      </ul>
+      <AddTodo onAdd={handleAdd} />
+    </section>
+  );
+}
