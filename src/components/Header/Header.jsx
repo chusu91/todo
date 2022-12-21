1fb4911369a13505878813a4d07db52e983a6034
Lucy <chusu91@gmail.com>
2022-12-21 21:51:06 +0200

    darkmode using Context

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,16 @@
 import React from "react";
-import { FaLifeRing } from "react-icons/fa";
+import { HiMoon, HiSun } from "react-icons/hi";
+import { useDarkMode } from "../../context/DarkModeContext";
 import styles from "./Header.module.css";
 
 export default function Header({ filters, filter, onFilterChange }) {
+  const { darkMode, toggleDarkMode } = useDarkMode();
   return (
     <header className={styles.header}>
+      <button onClick={toggleDarkMode} className={styles.toggle}>
+        {!darkMode && <HiMoon />}
+        {darkMode && <HiSun />}
+      </button>
       <ul className={styles.filters}>
         {filters.map((value, index) => (
           <li key={index}>
